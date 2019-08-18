c558562ab1c494200624a5af649e3707dcb5c44e
leonardo <leonardo.victorufpa@gmail.com>
2019-08-18 12:44:41 -0300

    Services Appointment

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,7 +21,11 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store); 
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
