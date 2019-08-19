1b63f323fa800a382f77af39141d6b52410ba7d8
leonardo <leonardo.victorufpa@gmail.com>
2019-08-19 17:20:10 -0300

    Schedule from providers

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,9 +25,11 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.get('/appointments', AppointmentController.index); 
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/appointments', AppointmentController.store); 
+
+routes.get('/schedule', ScheduleController.index); 
 
 routes.post('/files', upload.single('file'), FileController.store);
 
