80754825dd52da34180759a0d7b8e512c2da28a7
leonardo <leonardo.victorufpa@gmail.com>
2019-08-24 22:39:42 -0300

    List notification from user

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt'
 import Appointment from '../models/Appointments'
 import User from '../models/User';
 import File from '../models/File'; 
+import Notification from '../schemas/notification';
 
 class AppointmentController {
     async index(req, res){
@@ -51,6 +53,9 @@ class AppointmentController {
         /**
          * Check if provider_id is a provider
          */
+
+        
+
         const isProvider = await User.findOne({ 
             where: {
                 id: provider_id, provider: true
@@ -93,7 +98,23 @@ class AppointmentController {
             user_id: req.userId,
             provider_id,
             date,
-        })
+        });
+
+        /**
+         * Notify appointment provider
+         */
+
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMM' às' H:mm'h'",
+            { locale: pt } 
+            );
+
+        await Notification.create({
+            content:`Novo agendamento de ${user.name} para o  ${formattedDate}`,
+            user: provider_id,
+        });
 
         return res.json(appointment);
     };
