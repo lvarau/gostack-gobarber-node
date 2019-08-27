e2341fd9f6b0d7fa18df9106253d56a272085c57
leonardo <leonardo.victorufpa@gmail.com>
2019-08-26 23:22:48 -0300

    Nodemailer settings

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointments'
 import User from '../models/User';
 import File from '../models/File'; 
 import Notification from '../schemas/notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
@@ -121,7 +122,15 @@ class AppointmentController {
 
     async delete(req, res) {
         
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+            {
+                model: User,
+                as: 'provider',
+                attributes: ['name', 'email'],
+            },
+        ],
+        });
         
         if( appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -134,14 +143,21 @@ class AppointmentController {
         if(isBefore(dateWithSub, new Date())){
             return res.status(401).json({
                 error: 'You can only cancel appointments 2 hours in advance.'
-            });
+            }) ;
         };
 
         appointment.canceled_at = new Date();
 
         await appointment.save();
 
+         await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            text: 'Você tem um novo cancelamento',
+        })
+
         return res.json(appointment);
+        
     };
 };
 
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+    user: '8818c2e9598659',
+    pass: 'd65b47c1f70d40'
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
